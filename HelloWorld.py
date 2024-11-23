36749484114d2b4747f4ab31114a46aa92cf797d
naveenkumaranthony31 <naveenthiwary20@gmail.com>
2024-11-23 10:03:34 +0530

    python

---
+++ HelloWorld.py
@@ -0,0 +1,49 @@
+# Python printing Statment
+print('HelloWorld')
+
+#end and spration argument
+
+# defult \n print new line
+# end="" not print new line and print same line 
+print("2+3",end="")
+print(2+3)
+
+#spreteion 
+#spration is provied words space using , 
+# , use panna na defult da space 
+print("mic","phone","headset")
+# . varanu na sep='.'
+print("mic","phone","headset",sep='.')
+
+#secssino Task
+print("Hi","Hi", "",sep='...',end='')
+print("Hi")
+# or
+print("Hi","Hi", sep='...',end='...')
+print("Hi")
+
+#Escape sequence 
+#end='' argument defult value \n 
+#\n move to new line  
+print("") #empty new line
+# \n
+print("Escape\nSequences")
+# \t
+print("Escape\tSequences") #Tab space varum
+#\
+# print('thhis is pranava'phone') ERROR
+print('this is pranava\'s phone')
+# print("he said, "This is my phone"") ERROR
+print("he said,\"This is my phone\"")
+print('he said,"my phone"')
+print("he said,'my phone'")
+# print("this is blaklase:\") ERROR
+print("this is blaklase :\\")
+
+
+
+
+
+
+
+
